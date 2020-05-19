a77dacabe34dd0df163b29e96fc55f1d48dfcdb9
kurmorootsi <kurmo.rootsi@hotmail.com>
2020-05-19 20:14:38 +0300

    sss

--- exe.cs
+++ exe.cs
@@ -5,15 +5,28 @@ using System.Linq;
 
 class Failipunktid
 {
-    public static void Main(string[] arg)
-    {
-          punktid();
-    }
-
-
-
-	public static void punktid() {
-Console.WriteLine(File.ReadAllLines("punktid.txt").Select(p => double.Parse(p))
-		      .OrderBy(p => p).Skip(1).Take(3).Average());
+	public static void Main(string[] arg)
+	{
+		//inimestePikkus();
+		inimesteMassiiv();
 	}
+
+	public static void inimestePikkus() {
+		Console.WriteLine(File.ReadAllLines("pikkused.txt").Select(p => double.Parse(p))
+		      .OrderBy(p => p).Take(1));
+	}
+
+	public static void inimesteMassiiv() {
+		string[] eesnimed = { "Juku", "Kati", "Katrin", "Madis" };
+		IEnumerable<string> vastus=eesnimed.Select(Sorenda);
+
+		foreach(String element in eesnimed){
+			char[] item = element.ToCharArray();
+			
+        } 
+	}
+
+    public static string Sorenda(string tekst) {
+      return string.Join(" ", tekst.ToCharArray());
+    }
 }
---
+++ jalgpall.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.IO;
+public class Jalgpall{
+  public static void Main(string[] argumendid){
+
+    var players=File.ReadAllLines("jalgpall.txt").Select(rida => rida.Split(' ')).
+        Select(m => new{pknimi=m[0], goals=int.Parse(m[1])});
+
+    var playersSortedByGoal=players.OrderBy(player => player.goals);
+
+    Console.WriteLine(
+        players
+            .OrderBy(player => player.goals)
+            .Take(1)
+        );
+
+
+    File.WriteAllLines("sortgoals.txt", 
+        lapsedPikkusteJarjekorras.Select(player => player.pknimi+" on löönud "+int.Parse(player.goals*0.8)+" klubiväravat"));
+
+  }
+}
