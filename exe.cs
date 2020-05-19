5a5a521097e803a0c7cca181ef6a51b3057a56dc
kurmorootsi <kurmo.rootsi@hotmail.com>
2020-05-19 19:41:11 +0300

    tere

--- exe.cs
+++ exe.cs
@@ -7,7 +7,13 @@ class Failipunktid
 {
     public static void Main(string[] arg)
     {
-          Console.WriteLine(File.ReadAllLines("punktid.txt").Select(p => double.Parse(p))
-		      .OrderBy(p => p).Skip(1).Take(3).Average());
+          punktid();
     }
+
+
+
+	public static void punktid() {
+Console.WriteLine(File.ReadAllLines("punktid.txt").Select(p => double.Parse(p))
+		      .OrderBy(p => p).Skip(1).Take(3).Average());
+	}
 }
