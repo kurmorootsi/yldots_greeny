e5921cf1eb19691c6e816e83417f0d4bab6f5b1d
kurmorootsi <kurmo.rootsi@hotmail.com>
2020-05-20 14:24:58 +0300

    ssss

--- exe.cs
+++ exe.cs
@@ -8,7 +8,8 @@ class Failipunktid
 	public static void Main(string[] arg)
 	{
 		//inimestePikkus();
-		inimesteMassiiv();
+		//inimesteMassiiv();
+		inimesteTabelMassiiv();
 	}
 
 	public static void inimestePikkus() {
@@ -22,9 +23,20 @@ class Failipunktid
 
 		foreach(String element in eesnimed){
 			char[] item = element.ToCharArray();
-			
+
         } 
 	}
+
+	public static void inimesteTabelMassiiv() {
+		var inimene_linn=File.ReadAllLines("pikkused_tabel.txt").Select(rida => rida.Split(' ')).
+			Select(m => new{eesnimi=m[0], linn=m[1]});
+
+		var linn_sorted=inimene_linn.OrderBy(linnad => -linnad.linn);
+
+		    File.WriteAllLines("linnad_sorted.txt", 
+        linn_sorted.Select(linnad => linnad.linn));
+	}
+
 
     public static string Sorenda(string tekst) {
       return string.Join(" ", tekst.ToCharArray());
